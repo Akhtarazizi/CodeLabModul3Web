d2f109dedf16423e99c97c2b57011d46bc884717
Akhtarazizi <akhtarazizifarid@gmail.com>
2023-10-26 19:28:36 +0700

    CodelabModul3Web

---
+++ kegiatan-2/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h1> Bagian Footer</h1>
+      <p>&copy; 2023 My Website</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ kegiatan-2/src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Ini Tugas Latihan Praktikum Web Modul 3.......</p>
+    </div>
+  );
+}
+
+export default AboutUs;
